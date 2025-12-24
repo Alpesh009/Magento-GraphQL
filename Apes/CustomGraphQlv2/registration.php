85d5d8d2d5ad2459526fd0af0e7a29f37aec7796
Alpesh Solanki <40392553+Alpesh009@users.noreply.github.com>
2025-12-24 11:25:43 +0530

    Create registration.php

---
+++ Apes/CustomGraphQlv2/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Registration file for the custom GraphQL module.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Apes_CustomGraphQlv2',
+    __DIR__
+);
